963c47952b6ddb1076acaecb4ade71d661d4caf5
William ML Leslie <william.leslie.ttg@gmail.com>
2017-08-29 18:05:54 +1000

    Services for updating and starting the pager application

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# copy service files to system locations
-cp /home/pi/pager.service /lib/systemd/system/pager.service
-cp /home/pi/update.service /lib/systemd/system/update.service
-
-# install new services
-systemctl enable pager pager-update
-systemctl start pager
---
+++ updates/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+UNITS=/home/pi/pagerpi/systemd
+SERVICE_DEF=/lib/systemd/system
+
+# copy service files to system locations
+cp $UNITS/pager.service        $SERVICE_DEF/pager.service
+cp $UNITS/pager-update.service $SERVICE_DEF/pager-update.service
+cp $UNITS/pager-update.timer   $SERVICE_DEF/pager-update.timer
+
+# install new services
+systemctl enable pager pager-update
+systemctl start pager
