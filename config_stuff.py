714a59b8c9fb4ac0b6d6708f6942a357b4dc72ff
William ML Leslie <william.leslie.ttg@gmail.com>
2017-09-21 17:32:22 +1000

    Make status reports optional

--- config_stuff.py
+++ config_stuff.py
@@ -25,21 +25,6 @@ def fail_and_exit(text):
     sys.exit(text)
 
 
-def get_remote_url():
-    try:
-        with open('/home/pi/arena_remote_url') as f:
-            remote = f.read().strip()
-    except OSError:
-        fail_and_exit("Please set the contents of ~/arena_remote_url to "
-                      "a pager endpoint.\n"
-                      "e.g. http://arenatest.nafc.org.au/register/api/pddPager\n"
-                      "or http://10.0.0.130:8880")
-
-    if '://' not in remote:
-        fail_and_exit("Invalid ARENA_REMOTE: missing protocol (eg http://)")
-    return remote
-
-
 def form(d):
     return {'content' : json.dumps(d)}
 
@@ -65,7 +50,9 @@ def startup(app):
     """
     try:
         configure(app)
-#        requests.post(remote + "/startup", data=form(app.config))
+        report_url = app.config.get('reportUrl', '')
+        if report_url:
+            requests.post(report_url + "/startup", data=form(app.config))
     except Exception as e:
         with open(FAIL_LOG, 'a') as f:
             f.write("%s\n" % datetime.now())
@@ -78,8 +65,9 @@ def report(app):
     data = dict(app.status)
     now = data['report_time'] = str(datetime.now())
     try:
-        remote = get_remote_url()
-        res = requests.post(remote + "/report", data=form(data))
+        report_url = app.config.get('reportUrl', None)
+        if remote:
+            res = requests.post(report_url + "/report", data=form(data))
         res.raise_for_status()
     except (OSError, requests.exceptions.HTTPError) as e:
         app.status['errors'].append(e.message)
